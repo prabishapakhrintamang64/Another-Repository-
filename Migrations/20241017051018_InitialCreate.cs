b27ce9fe9eb3872a97e45dc4e92c9189853df667
prabisha <prabishapakhrin@gmail.com>
2024-10-16 22:21:03 -0700

    Added Changes

---
+++ Migrations/20241017051018_InitialCreate.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employy",
+                columns: table => new
+                {
+                    EmployyId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EmployyName = table.Column<string>(type: "TEXT", nullable: false),
+                    DepartId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employy", x => x.EmployyId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployyLeave",
+                columns: table => new
+                {
+                    LeaveId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EmployyId = table.Column<int>(type: "INTEGER", nullable: false),
+                    NumOfDays = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployyLeave", x => x.LeaveId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employy");
+
+            migrationBuilder.DropTable(
+                name: "EmployyLeave");
+        }
+    }
+}
